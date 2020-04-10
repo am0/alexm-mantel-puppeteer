87579c2702ea11b75e34c69e9b53c2f7370940bf
Alex Moyler <alex@alexm.co>
2020-04-10 13:16:51 +0100

    Take two, woo

--- screenshot.js
+++ screenshot.js
@@ -1,18 +1,27 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+const { parse } = require('url');
+const { getScreenshot } = require('./chromium');
+const { getInt, getUrlFromPath, isValidUrl } = require('./validator');
 
-async function getScreenshot(url, type) {
-	const browser = await puppeteer.launch({
-		args: chrome.args,
-		executablePath: await chrome.executablePath,
-		headless: chrome.headless,
-	});
-
-	const page = await browser.newPage();
-	await page.goto(url);
-	const file = await page.screenshot({ type });
-	await browser.close();
-	return file;
-}
-
-module.exports = getScreenshot;
+module.exports = async function (req, res) {
+	try {
+		const { pathname = '/', query = {} } = parse(req.url, true);
+		const { type = 'png', quality, fullPage } = query;
+		const url = getUrlFromPath(pathname);
+		const qual = getInt(quality);
+		if (!isValidUrl(url)) {
+			res.statusCode = 400;
+			res.setHeader('Content-Type', 'text/html');
+			res.end(`<h1>Bad Request</h1><p>The url <em>${url}</em> is not valid.</p>`);
+		} else {
+			const file = await getScreenshot(url, type, qual, fullPage);
+			res.statusCode = 200;
+			res.setHeader('Content-Type', `image/${type}`);
+			res.end(file);
+		}
+	} catch (e) {
+		res.statusCode = 500;
+		res.setHeader('Content-Type', 'text/html');
+		res.end('<h1>Server Error</h1><p>Sorry, there was a problem</p>');
+		console.error(e.message);
+	}
+};
